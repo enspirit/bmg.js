9b74db6b7588310f9390bf98928f19d3d28da1c2
Louis Lambeau <louis.lambeau@enspirit.be>
2025-04-20 10:55:10 +0200

    wip

--- src/types.ts
+++ src/types.ts
@@ -11,7 +11,6 @@ export interface Relation<T> {
   toArray(): Tuple<T>[]
 }
 
-
 export type RelationOperand<T> = Relation<T>|Tuple<T>[]
 
 export interface OperationalOperand<T> {
@@ -26,10 +25,13 @@ export type RenamingFunc<T> = (k: AttrName<T>) => string
 export type RenamingObj<T> = Partial<Record<AttrName<T>, string>>
 export type Renaming<T> = RenamingObj<T> | RenamingFunc<T>
 
-
-export type RenamedTuple<T, R extends Renaming<T>> =
-  R extends RenamingFunc<T>
-    ? RenameWithFunc<T, R, keyof T>
+export type RenamedTuple<T, R> =
+  R extends [infer K, infer F]
+    ? K extends keyof T
+      ? F extends (k: K) => string
+        ? RenameWithFunc<T, K, F>
+        : never
+      : never
     : R extends RenamingObj<T>
       ? RenameWithObj<T, R>
       : never
@@ -40,13 +42,9 @@ export type RenameWithObj<T, R extends RenamingObj<T>> = {
     : K]: T[K]
 }
 
-export type RenameWithFunc<T, F extends RenamingFunc<T>, K extends keyof T> = {
+export type RenameWithFunc<T, K extends keyof T, F extends (attr: K) => string> = {
   [P in keyof T as P extends K ? ReturnType<F> : P]: T[P]
 }
 
 export type RenamedRelation<T, R extends Renaming<T>> =
-  R extends (infer F extends RenamingFunc<T>)
-    ? Relation<RenameWithFunc<T, F, keyof T>>
-    : R extends RenamingObj<T>
-      ? Relation<RenameWithObj<T, R>>
-      : never
+  Relation<RenamedTuple<T, R>>
