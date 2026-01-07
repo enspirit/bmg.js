1fe016897458e8d1472bbf40490fbd1b4307d1dd
Bmg Developer <bmg@example.com>
2026-01-07 12:30:49 +0100

    Switch to vitest 4.x

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      'src': path.resolve(__dirname, 'src'),
+      'tests': path.resolve(__dirname, 'tests'),
     },
   },
   test: {
